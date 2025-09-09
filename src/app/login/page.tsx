68c63119d4ff2af897865e76597ead003627148a
edesbois77 <edesbois@yahoo.co.uk>
2025-09-09 18:36:19 +0100

    Fix cookies called outside request scope error

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import AuthForm from '@/components/AuthForm';
 import { getCurrentUser } from '@/lib/auth';
 import { redirect } from 'next/navigation';
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic';
+
 import { getCurrentUser } from '@/lib/auth';
 import { redirect } from 'next/navigation';
 
